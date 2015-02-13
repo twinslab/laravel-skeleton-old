a4ff55a29455260a159ff428d6665ff1a7307b7a
Nikos Gr <grrnikos@yahoo.gr>
2015-02-13 16:53:50 +0200

    redirect trailing slashes

---
+++ app/Http/Middleware/RedirectTrailingSlashes.php
@@ -0,0 +1,23 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+
+class RedirectTrailingSlashes
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if (preg_match('/.+\/$/', $request->getRequestUri()))
+		{
+			return redirect(rtrim($request->getRequestUri(), '/'), 301);
+        }
+
+		return $next($request);
+	}
+}
