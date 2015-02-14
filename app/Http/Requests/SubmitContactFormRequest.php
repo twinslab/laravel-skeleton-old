ff1087778a1696e3801f9713f5577a6c49ef7557
Nikos Gr <grrnikos@yahoo.gr>
2015-02-14 12:27:58 +0200

    flash messages ready

--- app/Http/Requests/SubmitContactFormRequest.php
+++ app/Http/Requests/SubmitContactFormRequest.php
@@ -1,5 +1,8 @@
 <?php namespace App\Http\Requests;
 
+use Illuminate\Support\Facades\Session;
+use Lang;
+
 class SubmitContactFormRequest extends Request
 {
 	/**
@@ -24,5 +27,20 @@ class SubmitContactFormRequest extends Request
 			'email' => 'required|email',
 			'message' => 'required|min:5'
 		];
+	}
+
+
+	/**
+	 * Overwrite the response method so we are able to pass a custom error message.
+	 *
+	 * {@inheritdoc}
+     */
+	public function response(array $errors)
+	{
+		Session::flash('alert-danger', Lang::get('pages/contact.form.error'));
+
+		return $this->redirector->to($this->getRedirectUrl())
+			->withInput($this->except($this->dontFlash))
+			->withErrors($errors, $this->errorBag);
 	}
 }
--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -8,14 +8,3 @@
         </div>
     @endif
 @endforeach
-
-@if($errors->any())
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert"></button>
-        <ul class="list-unstyled">
-            @foreach($errors->all() as $error)
-                <li>{!! $error !!}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
